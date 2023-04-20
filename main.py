fe46e2c0825576f81b1c46a100ed57c129378b4c
Ricardo Castro Vizcaya <36154504+RicardoCVS@users.noreply.github.com>
2023-04-21 00:32:06 +0200

    Codigo completo en Python

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from PIL import Image
 
 # Asegúrate de tener instalado el ChromeDriver
 driver = webdriver.Chrome()
@@ -35,35 +34,27 @@ wiki_link.click()
 
 time.sleep(3)  # Esperar a que se cargue la página de Wikipedia
 
-# Buscar el párrafo usando el XPath
+# Buscar el párrafo usando el XPath proporcionado
 paragraph = driver.find_element(By.XPATH, "//*[@id='mw-content-text']/div[1]/p[28]")
-paragraph_text = paragraph.text
 
 # Hacer scroll hasta el elemento paragraph
 driver.execute_script("arguments[0].scrollIntoView();", paragraph)
 
-# Extraer los años (números de cuatro dígitos) del párrafo
+# Esperar un tiempo adicional para asegurar que la página esté completamente renderizada
+time.sleep(3)
+
+paragraph_text = paragraph.text
+
+# Extraer los años (números de tres y cuatro dígitos) del párrafo
 years_four_digits = re.findall(r'\b\d{4}\b', paragraph_text)
 years_three_digits = re.findall(r'\b\d{3}\b', paragraph_text)
 
-print(f"Años encontrados en el párrafo con 4 digitos: {', '.join(years_four_digits)}")
-print(f"Años encontrados en el párrafo con 3 digitos: {', '.join(years_three_digits)}")
+print(f"Años encontrados en el párrafo con 4 dígitos: {', '.join(years_four_digits)}")
+print(f"Años encontrados en el párrafo con 3 dígitos: {', '.join(years_three_digits)}")
 
-
-#Para guardar la imagen recortada solo del parrafo hay que instalar pillow: 'pip install Pillow'
 # Guardar screenshot completo
-driver.save_screenshot("wikipedia_page.png")
-
-# Recortar la imagen para incluir solo el párrafo
-image = Image.open("wikipedia_page.png")
-location = paragraph.location
-size = paragraph.size
-area = (location['x'], location['y'], location['x'] + size['width'], location['y'] + size['height'])
-cropped_image = image.crop(area)
-
-# Guardar el screenshot del párrafo en el escritorio
 desktop_path = "C:\\Users\\vicar\\OneDrive\\Desktop"
-with open(os.path.join(desktop_path, "wikipedia_paragraph.png"), "wb") as f:
-    cropped_image.save(f, format="PNG")
+with open(os.path.join(desktop_path, "wikipedia_page.png"), "wb") as f:
+    f.write(driver.get_screenshot_as_png())
 
 driver.quit()
