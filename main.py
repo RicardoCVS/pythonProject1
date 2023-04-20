e7be593c5099ac3f9b38ab62140af2c5f06e1a1f
Ricardo Castro Vizcaya <36154504+RicardoCVS@users.noreply.github.com>
2023-04-20 23:27:23 +0200

    Añadido tiempo de espera y xpath correcto

--- main.py
+++ main.py
@@ -1,48 +1,40 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
-import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Configura el driver
-driver_path = os.environ.get('WEBDRIVER_PATH', None)
-if driver_path is None:
-    raise Exception("No se encontró el path del driver. Asegúrate de configurar la variable de entorno 'WEBDRIVER_PATH'")
-driver = webdriver.Chrome(driver_path)
+# Asegúrate de tener instalado el ChromeDriver
+driver = webdriver.Chrome()
 
-# Busca en Google
 driver.get("https://www.google.com")
+
+# Aceptar cookies
 try:
-    accept_cookies_button = driver.find_element_by_css_selector(".consent-bump__actions > button")
-    accept_cookies_button.click()
+    cookies_accept_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//*[@id='L2AGLb']/div"))
+    )
+    cookies_accept_button.click()
 except Exception as e:
-    print("No se encontró el botón de aceptar cookies:", e)
+    print("No se encontró el botón para aceptar las cookies. Omitiendo...")
 
-search_box = driver.find_element_by_name("q")
-search_box.send_keys("automatización")
-search_box.send_keys(Keys.RETURN)
-time.sleep(2)
+time.sleep(10)  # Esperar a que se carguen los resultados
 
-# Encuentra el enlace de Wikipedia
-search_results = driver.find_elements_by_css_selector(".g .tF2Cxc")
-wikipedia_link = None
-for result in search_results:
-    link = result.find_element_by_css_selector(".yuRUbf > a")
-    if "wikipedia.org" in link.get_attribute("href"):
-        wikipedia_link = link
-        break
+search_bar = driver.find_element(By.XPATH,'//*[@id="APjFqb"]')
+search_bar.send_keys("automatización")
+search_bar.send_keys(Keys.RETURN)
 
-if wikipedia_link is not None:
-    # Navega a la página de Wikipedia y obtén el año del primer proceso automático
-    wikipedia_link.click()
-    time.sleep(2)
-    year_element = driver.find_element_by_css_selector(".infobox tr:nth-child(3) td")
-    year = year_element.text
-    print(f"El primer proceso automático fue en el año {year}.")
+time.sleep(3)  # Esperar a que se carguen los resultados
 
-    # Realiza un screenshot de la página de la Wikipedia
-    driver.save_screenshot("wikipedia_page.png")
-else:
-    print("No se encontró el enlace de Wikipedia en los resultados de búsqueda.")
+wiki_link = driver.find_element(By.XPATH, "//a[contains(@href, 'https://es.wikipedia.org')]")
+wiki_link.click()
 
+time.sleep(3)  # Esperar a que se cargue la página de Wikipedia
+
+year = driver.find_element(By.XPATH, "//*[contains(text(), 'primer proceso automático')]").text
+
+print(f"Año en que se realizó el primer proceso automático: {year}")
+
+driver.save_screenshot("wikipedia_page.png")
 driver.quit()
-
